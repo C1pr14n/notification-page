8ee574572066aa0f2f7a452f9ec4db9a2f9ecfe1
GeorgeCiprian <cipriant95@gmail.com>
2023-02-09 14:35:08 +0200

    update notifications

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const btn = document.querySelector(".header-btn");
 const notifications = document.querySelectorAll(".notification");
 const notificationNr = document.getElementById("notifications-number");
 
+const unreadMessages = document.querySelectorAll(".unread");
+const span = document.querySelectorAll(".bullet");
+
+// Unread Notification
 const unreadNotifications = Array.from(notifications).filter((el) =>
   el.classList.contains("notification-unread")
 );
-
 notificationNr.textContent = unreadNotifications.length;
 
-btn.addEventListener("click", (e) => {
+// Mark all as read
+btn.addEventListener("click", () => {
   notifications.forEach((el) => el.classList.remove("notification-unread"));
   notificationNr.textContent = 0;
+  span.forEach((el) => el.classList.remove("dot"));
 });
